bd506b3ae06a100827ffba4df5e5b0e335087c4b
Maria79 <mariajalbisu@gmail.com>
2023-01-30 15:04:37 +0000

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,6 @@ export async function getServerSideProps(context) {
 	const products = await fetch('https://fakestoreapi.com/products').then(
 		(res) => res.json()
 	);
-	// .then((json) => console.log(json));
 
 	return {
 		props: {
